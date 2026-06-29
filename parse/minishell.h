1ca25f37a98709ba989883acd17ca41fdfaf130d
Satoru Hirose <shirose@c3r4p1.42vienna.com>
2026-06-29 16:47:37 +0200

    added #endif

--- parse/minishell.h
+++ parse/minishell.h
@@ -7,7 +7,7 @@ typedef enum e_redir_type
 	redir_input,	// '<'
 	redir_output,	// '>'
 	redir_append,	// '>>'
-	redir_heredoc,	// '<<'
+	redir_heredoc	// '<<'
 } t_redir_type;
 
 typedef enum e_operator
@@ -16,17 +16,19 @@ typedef enum e_operator
 	op_pipe		// '|'
 }	t_operator;
 
-typedef struct s_redir
+typedef struct s_redir_lst
 {
 	t_redir_type	type;		// 
 	char			*filename;  // File to open/read/write
 	struct s_redir	*next;		// Next redirection, if any
-}	t_redir;
+}	t_redir_lst;
 
-typedef struct	s_cmd
+typedef struct	s_cmd_lst
 {
-	char		**argv;		// Args for execve
-	t_redir		*redirs;	// How this cmd redirects
-	t_operator	op;			// None or pipe
-	t_cmd		*next;		// Next cmd
-} t_cmd;
+	char				**argv;		// Args for execve
+	t_redir_lst			*redirs;	// How this cmd redirects
+	t_operator			op;			// None or pipe
+	struct s_cmd_lst	*next;		// Next cmd
+} t_cmd_lst;
+
+#endif
